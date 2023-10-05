194d8a2271ab89be912e752915f60b4434c11b0b
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-10-06 07:09:03 +0800

    adsf

---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,14 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+export default function Expenses(props) {
+    return (
+        <Card className="expenses">
+            <ExpenseItem expense={props.expenses[0]} />
+            <ExpenseItem expense={props.expenses[1]} />
+            <ExpenseItem expense={props.expenses[2]} />
+            <ExpenseItem expense={props.expenses[3]} />
+        </Card>
+    );
+}
