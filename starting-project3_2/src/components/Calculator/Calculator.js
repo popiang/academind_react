1746613af5041cd35545f61ecfe1662793acd44e
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-10-18 23:12:26 +0800

    done and done

--- starting-project3_2/src/components/Calculator/Calculator.js
+++ starting-project3_2/src/components/Calculator/Calculator.js
@@ -1,18 +1,58 @@
+import { useState } from "react";
 import styles from "./Calculator.module.css";
 
-export default function Calculator() {
+let initialData = {
+    "current-savings": 10000,
+    "yearly-contribution": 500,
+    "expected-return": 7,
+    duration: 5,
+};
+
+export default function Calculator({ onCalculate }) {
+    const [userInput, setUserInput] = useState(initialData);
+
+    const handleResetClick = () => {
+        setUserInput(initialData);
+		onCalculate(null);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+		onCalculate(userInput);
+    };
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <div className={styles["input-group"]}>
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings" />
+                    <input
+                        type="number"
+                        id="current-savings"
+                        onChange={(e) =>
+                            setUserInput({
+                                ...userInput,
+                                "current-savings": e.target.value,
+                            })
+                        }
+                        value={userInput["current-savings"]}
+                    />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">
                         Yearly Savings ($)
                     </label>
-                    <input type="number" id="yearly-contribution" />
+                    <input
+                        type="number"
+                        id="yearly-contribution"
+                        onChange={(e) =>
+                            setUserInput({
+                                ...userInput,
+                                "yearly-contribution": e.target.value,
+                            })
+                        }
+                        value={userInput["yearly-contribution"]}
+                    />
                 </p>
             </div>
             <div className="input-group">
@@ -20,17 +60,41 @@ export default function Calculator() {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input type="number" id="expected-return" />
+                    <input
+                        type="number"
+                        id="expected-return"
+                        onChange={(e) =>
+                            setUserInput({
+                                ...userInput,
+                                "expected-return": e.target.value,
+                            })
+                        }
+                        value={userInput["expected-return"]}
+                    />
                 </p>
                 <p>
                     <label htmlFor="duration">
                         Investment Duration (years)
                     </label>
-                    <input type="number" id="duration" />
+                    <input
+                        type="number"
+                        id="duration"
+                        onChange={(e) =>
+                            setUserInput({
+                                ...userInput,
+                                duration: e.target.value,
+                            })
+                        }
+                        value={userInput.duration}
+                    />
                 </p>
             </div>
             <p className={styles.actions}>
-                <button type="reset" className={styles.buttonAlt}>
+                <button
+                    type="reset"
+                    className={styles.buttonAlt}
+                    onClick={handleResetClick}
+                >
                     Reset
                 </button>
                 <button type="submit" className={styles.button}>
--- starting-project3_2/src/components/OutputTable/OutputTable.js
+++ starting-project3_2/src/components/OutputTable/OutputTable.js
@@ -1,6 +1,13 @@
 import styles from "./OutputTable.module.css";
 
-export default function OutputTable() {
+const formatter = new Intl.NumberFormat("en-US", {
+	style: "currency",
+	currency: "USD",
+	minimumFractionDigits: 2,
+	maximumFractionDigits: 2
+})
+
+export default function OutputTable({ results, initialInvestment }) {
     return (
         <table className={styles.result}>
             <thead>
@@ -13,13 +20,22 @@ export default function OutputTable() {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>YEAR NUMBER</td>
-                    <td>TOTAL SAVINGS END OF YEAR</td>
-                    <td>INTEREST GAINED IN YEAR</td>
-                    <td>TOTAL INTEREST GAINED</td>
-                    <td>TOTAL INVESTED CAPITAL</td>
-                </tr>
+                {results.map((result) => (
+                    <tr key={result.year}>
+                        <td>{result.year}</td>
+                        <td>{formatter.format(result.savingsEndOfYear)}</td>
+                        <td>{formatter.format(result.yearlyInterest)}</td>
+                        <td>
+                            {formatter.format(result.savingsEndOfYear -
+                                initialInvestment -
+                                result.yearlyContribution * result.year)}
+                        </td>
+                        <td>
+                            {formatter.format(initialInvestment +
+                                result.yearlyContribution * result.year)}
+                        </td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
