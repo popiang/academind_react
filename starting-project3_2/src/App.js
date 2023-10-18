a440b266bc7517c88a78217c40993b006525e6e1
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-10-18 16:35:28 +0800

    asdf

--- starting-project3_2/src/App.js
+++ starting-project3_2/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Calculator from "./components/Calculator/Calculator";
+import OutputTable from "./components/OutputTable/OutputTable";
 
 function App() {
     const calculateHandler = (userInput) => {
@@ -33,31 +34,7 @@ function App() {
         <div>
 			<Header />
 			<Calculator />
-           
-
-            {/* Todo: Show below table conditionally (only once result data is available) */}
-            {/* Show fallback text if no data is available */}
-
-            <table className="result">
-                <thead>
-                    <tr>
-                        <th>Year</th>
-                        <th>Total Savings</th>
-                        <th>Interest (Year)</th>
-                        <th>Total Interest</th>
-                        <th>Invested Capital</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>YEAR NUMBER</td>
-                        <td>TOTAL SAVINGS END OF YEAR</td>
-                        <td>INTEREST GAINED IN YEAR</td>
-                        <td>TOTAL INTEREST GAINED</td>
-                        <td>TOTAL INVESTED CAPITAL</td>
-                    </tr>
-                </tbody>
-            </table>
+			<OutputTable />
         </div>
     );
 }
